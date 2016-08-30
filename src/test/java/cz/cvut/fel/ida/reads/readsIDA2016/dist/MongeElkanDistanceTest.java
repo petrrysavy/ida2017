01ae309e490d5b8728fd809cc7fc9a047a3bd6cf
Petr Ryšavý <rysavpe1@fel.cvut.cz>
2016-08-30 16:19:51 +0200

    Added GNU GPL 3 license to the code

--- src/test/java/cz/cvut/fel/ida/reads/readsIDA2016/dist/MongeElkanDistanceTest.java
+++ src/test/java/cz/cvut/fel/ida/reads/readsIDA2016/dist/MongeElkanDistanceTest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2016 Petr Rysavy <rysavpe1@fel.cvut.cz>
+ *
+ * This file is part of the project readsIDA2016, which is available on 
+ * <https://github.com/petrrysavy/readsIDA2016/>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with This program.  If not, see <http ://www.gnu.org/licenses/>.
+ */
 package cz.cvut.fel.ida.reads.readsIDA2016.dist;
 
 import cz.cvut.fel.ida.reads.readsIDA2016.model.HashMultiset;
